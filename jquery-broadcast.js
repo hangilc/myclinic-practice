c7c2074319b200d45f81e78173e895a0e5208cd3
Hangil Chang <hangil@chang.jp>
2016-08-09 20:26:44 +0900

    dmodified jquery-broadcast

--- jquery-broadcast.js
+++ jquery-broadcast.js
@@ -3,30 +3,21 @@
 var $ = require("jquery");
 
 $.fn.broadcast = function(key, args){
-	console.log("enter broadcast", key);
 	if( args === undefined ){
 		args = [];
 	}
-	var retval = this.map(function(){
+	return this.map(function(){
 		var e = $(this);
-		console.log("listeners", e.find("." + key).length);
 		var iterReturn = e.find("." + key).map(function(){
-			var listener = $(this);
+			return $(this);
 			var cb = listener.data(key);
 			if( typeof cb === "function" ){
-				var listenReturn = cb.apply(listener, args);
-				console.log("listen returns", listenReturn);
-				listenReturn = 1;
-				return listenReturn;
+				return cb.apply(listener, args);
 			} else {
 				throw new Exception("cannot find function while broadcasting: " + key);
 			}
 		}).get();
-		console.log("iter returns", iterReturn);
-		return iterReturn;
 	}).get();
-	console.log("leave braodcast", key, retval);
-	return retval;
 };
 
 $.fn.listen = function(key, cb){
