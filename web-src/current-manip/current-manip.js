99e8360fccff90b59b38728f1c1359bbb5f6e763
hangilc <hangil@chang.jp>
2016-09-07 23:38:51 +0900

    implemented refer

--- web-src/current-manip/current-manip.js
+++ web-src/current-manip/current-manip.js
@@ -6,6 +6,8 @@ var SearchText = require("./search-text");
 var modal = require("../../myclinic-modal");
 var task = require("../task");
 var service = require("../service");
+var kanjidate = require("kanjidate");
+var mUtil = require("../../myclinic-util");
 
 var tmplHtml = require("raw!./current-manip.html");
 
@@ -13,7 +15,7 @@ var accountLinkSelector = "[mc-name=accountButton]";
 var searchTextLinkSelector = "[mc-name=searchTextLink]";
 var referLinkSelector = "[mc-name=createReferLink]";
 
-exports.setup = function(dom){
+exports.setup = function(dom, referUrl){
 	var patientId = 0;
 	dom.listen("rx-start-page", function(appData){
 		patientId = appData.currentPatientId;
@@ -31,6 +33,44 @@ exports.setup = function(dom){
 		event.preventDefault();
 		doSearchText(patientId);
 	});
+	dom.on("click", referLinkSelector, function(event){
+		event.preventDefault();
+		var patient;
+		task.run([
+			function(done){
+				console.log(patientId);
+				service.getPatient(patientId, function(err, result){
+					if( err ){
+						done(err);
+						return;
+					}
+					patient = result;
+					done();
+				})
+			}
+		], function(err){
+			if( err ){
+				alert(err);
+				return;
+			}
+			var bday = (patient.birth_day && patient.birth_day === "0000-00-00") ? 
+				"" : kanjidate.format(kanjidate.f5, patient.birth_day);
+			var age = null;
+			if( patient.birth_day && patient.birth_day !== "0000-00-00" ){
+				age = mUtil.calcAge(patient.birth_day);
+			}
+			var data = {
+				"title": "紹介状",
+				"patient-name": patient.last_name + " " + patient.first_name,
+				"patient-birthday": bday,
+				"patient-age": age,
+				"patient-sex": mUtil.sexToKanji(patient.sex)
+			};
+			var form = event.target.closest("form");
+			form.querySelector("input[name=json-data]").value = JSON.stringify(data);
+			form.submit();
+		})
+	})
 	dom.on("click", "[mc-name=endPatientButton]", function(event){
 		event.preventDefault();
 		dom.trigger("end-patient");
